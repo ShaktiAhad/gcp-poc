a10067ba7b8ea83bb6da39123cceb60941152109
Ahad Noor Shakti <53283457+ShaktiAhad@users.noreply.github.com>
2022-05-25 02:54:06 +0900

    Create b.py

---
+++ b.py
@@ -0,0 +1,18 @@
+from google.cloud import dialogflowcx_v3beta1
+from google.oauth2 import service_account
+from google.cloud import storage
+import os
+
+credentials = service_account.Credentials.from_service_account_file('alien-hour-350703-c26ef39be23a.json')
+storage_client = storage.Client(credentials=credentials)
+
+output = os.popen('find /var/jenkins_home/workspace/gcp-pipeline/ -type f -name exported_agent_test.blob').read()
+d = output.strip()
+def upload_blob(bucket_name, source_file_name, destination_blob_name):
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+    blob.upload_from_filename(source_file_name)
+
+    print(f"File {source_file_name} uploaded to {destination_blob_name}.")
+
+upload_blob("dialogflow-bucket", d, "agent.blob")
