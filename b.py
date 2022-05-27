ad31920b4251cae5df8ebaa57cf0dd54b5c0585a
Ahad Noor Shakti <53283457+ShaktiAhad@users.noreply.github.com>
2022-05-28 03:47:13 +0900

    Delete b.py

--- b.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-credFile = sys.argv[1] # prerprod-project-dialogflowcx.json
-env = sys.argv[2] # preprod/prod
-version = sys.argv[3] # v0.0.0
-if env == "preprod":
-    blob_file_name = sys.argv[4] # exported_agent
-
-print(env, version)
