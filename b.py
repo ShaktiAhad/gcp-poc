111f32f5213fac0406552796474a7f5a684b8041
Ahad Noor Shakti <53283457+ShaktiAhad@users.noreply.github.com>
2022-05-28 03:13:14 +0900

    Update b.py

--- b.py
+++ b.py
@@ -1,19 +1,9 @@
-from google.cloud import dialogflowcx_v3beta1
-from google.oauth2 import service_account
-from google.cloud import storage
-import os, sys
+import sys
 
-credFile = sys.argv[1]
-credentials = service_account.Credentials.from_service_account_file(f'{credFile}')
-storage_client = storage.Client(credentials=credentials)
+credFile = sys.argv[1] # prerprod-project-dialogflowcx.json
+env = sys.argv[2] # preprod/prod
+version = sys.argv[3] # v0.0.0
+if env == "preprod":
+    blob_file_name = sys.argv[4] # exported_agent
 
-output = os.popen('find /var/jenkins_home/workspace/gcp-pipeline/ -type f -name exported_agent_test.blob').read()
-d = output.strip()
-def upload_blob(bucket_name, source_file_name, destination_blob_name):
-    bucket = storage_client.bucket(bucket_name)
-    blob = bucket.blob(destination_blob_name)
-    blob.upload_from_filename(source_file_name)
-
-    print(f"File {source_file_name} uploaded to {destination_blob_name}.")
-
-upload_blob("dialogflow-bucket", d, "agent.blob")
+print(env, version, blob_file_name)
