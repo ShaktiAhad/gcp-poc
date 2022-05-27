b500b29c546ef8ede0c3dad2037a0b2ed1dd68b2
Ahad Noor Shakti <53283457+ShaktiAhad@users.noreply.github.com>
2022-05-28 04:02:49 +0900

    Update upload_blob_github_to_preprod_bucket.py

--- upload_blob_github_to_preprod_bucket.py
+++ upload_blob_github_to_preprod_bucket.py
@@ -5,7 +5,7 @@ def upload_blob_to_preprod_bucket(bucket_name):
     version = arg.version
     blob_file_name = arg.blob_file_name
     storage_client = starting().storage_client 
-    output = os.popen(f'find /var/jenkins_home/workspace/gcp-pipeline/ -type f -name {blob_file_name}.blob').read()
+    output = os.popen(f'find /var/jenkins_home/workspace/gcp-pipeline -type f -name {blob_file_name}.blob').read()
     source_file_name = output.strip()
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(f"{blob_file_name}_{version}.blob")
