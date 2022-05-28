52380d63058f80c248da28b7e44d5989e8e60813
Ahad Noor Shakti <53283457+ShaktiAhad@users.noreply.github.com>
2022-05-28 13:49:07 +0900

    Update task_exec.py

--- task_exec.py
+++ task_exec.py
@@ -1,6 +1,7 @@
 import upload_blob_github_to_preprod_bucket, com_restore_agent, copy_blob_preprod_to_prod_bucket, arg
 
 def task_exec(env):
+    print(f"\n---> working on {env} env.")
     if env == "preprod":
         project_id = "prerprod-project"
         bucket_name = "dialogflow-bucket-preprod"
@@ -12,6 +13,6 @@ def task_exec(env):
         destination_bucket_name = "dialogflow-bucket-prod"
         copy_blob_preprod_to_prod_bucket.copy_blob_preprod_to_prod_bucket(source_bucket_name, destination_bucket_name)
         com_restore_agent.restore_agent(env, destination_bucket_name, project_id)
-    print("task is completed.")
+    print("---> __task is completed.__")
 
 task_exec(arg.env) 
