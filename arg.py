f04defa9b6fe49ec16b4c85771988c60bb362aa5
Ahad Noor Shakti <53283457+ShaktiAhad@users.noreply.github.com>
2022-05-28 13:34:01 +0900

    Update arg.py

--- arg.py
+++ arg.py
@@ -5,3 +5,6 @@ env = sys.argv[2] # preprod/prod
 version = sys.argv[3] # v0.0.0
 if env == "preprod":
     blob_file_name = sys.argv[4] # exported_agent
+    if blob_file_name.endswith(".blob"):
+        blob_file_name = blob_file_name.split(".blob").pop(-2)
+    else: blob_file_name=blob_file_name
